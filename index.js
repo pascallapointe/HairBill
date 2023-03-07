9567cef58793f198f6c180d5c7165d36958e30f1
pascallapointe <3847945+pascallapointe@users.noreply.github.com>
2023-03-07 10:54:14 -0500

    ReactNative update. Removed Hermes engine.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import { AppRegistry } from 'react-native';
 import './i18n';
 import App from './App';
 import { name as appName } from './app.json';
-
 import firestore from '@react-native-firebase/firestore';
+
 firestore()
   .settings({
     persistence: true,
